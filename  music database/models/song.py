19ca2889ae9de51158c61eb5d8ec54727d00fa4c
danik322 <danylobucharov@gmail.com>
2026-01-03 22:38:04 +0200

    added Optional to Song.album_id and Song.album, __repr__

---  music database/models/song.py
+++  music database/models/song.py
@@ -1,5 +1,5 @@
 from .base import Base
-from typing import List
+from typing import List, Optional
 from sqlalchemy import String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
@@ -11,4 +11,10 @@ class Song(Base):
     length: Mapped[int]
 
     mus_project_id: Mapped[int] = mapped_column(ForeignKey("musical_project.id"))
-    album_id: Mapped[int] = mapped_column(ForeignKey("album.id"))
+    album_id: Mapped[Optional[int]] = mapped_column(ForeignKey("album.id"))
+
+    musical_project: Mapped["Musical_project"] = relationship(back_populates="songs")
+    album: Mapped[Optional["Album"]] = relationship(back_populates="songs")
+
+    def __repr__(self):
+        return f"Song(name: {self.name}, id: {self.id}, lentgh: {self.length}, album_id: {self.album_id})"
