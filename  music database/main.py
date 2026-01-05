6cae82a72ae929006d1143d289a57c1e178c809b
danik322 <danylobucharov@gmail.com>
2026-01-05 13:13:44 +0200

    test of functions

---  music database/main.py
+++  music database/main.py
@@ -1,5 +1,25 @@
 from models import Artist, Musical_project, Album, Song, Instrument
 from database import engine, init_db
 from sqlalchemy.orm import Session
+import crud
 
-#init_db()
+init_db()
+
+
+with Session(engine) as session:
+    crud.create_artist(session, "Gerard Way")
+    session.commit()
+    print(crud.get_artist_by_name(session, "Gerard Way"))
+    crud.create_musical_project(session, "Gerard Way", [1], "Solo project")
+    session.commit()
+    print(crud.get_musical_project_by_name(session, "Gerard Way"))
+
+    crud.create_artist(session, "Ray Toro")
+    session.commit()
+    print(crud.get_artist_by_name(session, "Ray Toro"))
+
+    crud.create_song(session, "Baby youre a haunted house", 166, 1)
+    session.commit()
+
+    print(crud.get_song_by_name(session, "Baby youre a haunted house"))
+    
